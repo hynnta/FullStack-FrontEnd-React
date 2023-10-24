a013f690d9c558897f993e54ec66ab0d546efb9d
AnhTuan <1951052223tuan@ou.edu.vn>
2023-10-25 00:07:37 +0700

    create form login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './Login.scss'
 
 const Login = (props) => {
     return (
-        <div className='login-container'>
-            Login
+        <div className='login-container mt-3'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='conten-left col-6'>
+                        <div className='banner'>HYNNTA</div>
+                        <div className='detail'>learning reactjs with hynnta</div>
+                    </div>
+                    <div className='content-right col-4 d-flex flex-column gap-3 py-3'>
+                        <input className='form-control' type='text' placeholder='Email address or username'/>
+                        <input className='form-control' type='password' placeholder='password' />
+                        <button className='btn btn-primary'>Login</button>
+                        <span className='text-center'><u>Forgotten password?</u></span>
+                        <hr />
+                        <div className='text-center'>
+                            <button className='btn btn-success'>Create new account</button>
+                        </div>                       
+                    </div>
+                    <div className='col-2'></div>
+                </div>
+            </div>
         </div>
     );
 }
